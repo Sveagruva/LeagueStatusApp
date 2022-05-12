93d822e25217e183bb48304a5f8f22c22cbf86cb
Sveagruva <55844843+Sveagruva@users.noreply.github.com>
2022-05-12 20:42:13 +0400

    Initial Commit

---
+++ Shared/LeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeagueApp.swift
+//  Shared
+//
+//  Created by Sveagruva on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct LeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
