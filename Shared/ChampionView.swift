27dfc7df703fab14d1b043d51ff4b45b9b264ec6
Sveagruva <55844843+Sveagruva@users.noreply.github.com>
2022-06-18 21:09:33 +0400

    remove geometry reader and hardcode height

--- Shared/ChampionView.swift
+++ Shared/ChampionView.swift
@@ -46,153 +46,149 @@ struct ChampionView: View {
 	let AR = 0.8
 
 	var body: some View {
+
 		VStack {
-			GeometryReader { geo in
-				ScrollView {
-					VStack {
-						HStack {
+			ScrollView {
+				VStack {
+					HStack {
+						Spacer()
+						VStack {
+							if (selectedChampion != nil) {
+								Color.clear
+								  .background(
+									  RawDataImage(data: dta, origin: selectedChampion != nil)
+								  )
+								  .frame(height: 400)
+								  .clipped()
+								  .contentShape(Rectangle())
+							} else {
+								Color.clear
+								  .frame(height: 400)
+							}
+
+							if (selectedChampion != nil) {
+								Text(champion.name)
+								  .font(.system(size: 70, weight: .heavy, design: .default))
+								  .minimumScaleFactor(0.1)
+								  .matchedGeometryEffect(id: champion.id + "title", in: ns)
+								  .frame(maxWidth: .infinity, alignment: .topLeading)
+							} else {
+								Text(champion.name)
+								  .font(.system(size: 30, weight: .heavy, design: .default))
+								  .minimumScaleFactor(0.1)
+								  .opacity(0)
+							}
+
+
+
+
+							Text(champion.title)
+							  .font(.system(size: 36, weight: .bold, design: .default))
+							  .frame(maxWidth: .infinity, alignment: .topLeading)
 							Spacer()
-							VStack {
-								if (selectedChampion != nil) {
-									Color.clear
-									  .background(
-										  RawDataImage(data: dta, origin: selectedChampion != nil)
-									  )
-									  .frame(height: geo.size.height / 3 < 400 ? 400 : geo.size.height / 3)
-									  .clipped()
-									  .contentShape(Rectangle())
+							  .frame(height: 20)
 
-								} else {
-									Color.clear
-									  .frame(height: geo.size.height / 3 < 400 ? 400 : geo.size.height / 3)
-									  .clipped()
-									  .contentShape(Rectangle())
-								}
+							Text(champion.blurb)
+								//                                    .font(.system(size: 36, weight: .bold, design: .default))
+							  .frame(maxWidth: .infinity, alignment: .topLeading)
 
-								if (selectedChampion != nil) {
-									Text(champion.name)
-									  .font(.system(size: 70, weight: .heavy, design: .default))
-									  .minimumScaleFactor(0.1)
-									  .matchedGeometryEffect(id: champion.id + "title", in: ns)
-									  .frame(maxWidth: .infinity, alignment: .topLeading)
-								} else {
-									Text(champion.name)
-									  .font(.system(size: 30, weight: .heavy, design: .default))
-									  .minimumScaleFactor(0.1)
-									  .opacity(0)
-								}
-
-
-
-
-								Text(champion.title)
-								  .font(.system(size: 36, weight: .bold, design: .default))
-								  .frame(maxWidth: .infinity, alignment: .topLeading)
-								Spacer()
-								  .frame(height: 20)
-
-								Text(champion.blurb)
-									//                                    .font(.system(size: 36, weight: .bold, design: .default))
-								  .frame(maxWidth: .infinity, alignment: .topLeading)
-
-								if (d.fullDataChamp != nil) {
-									VStack {
-										Spacer().frame(height: 30)
+							if (d.fullDataChamp != nil) {
+								VStack {
+									Spacer().frame(height: 30)
 //                                        Text("Passive")
 //                                            .font(.system(size: 18, weight: .semibold, design: .default))
 //                                            .frame(maxWidth: .infinity, alignment: .topLeading)
+									HStack {
+										AsyncImage(url: URL(string: d.fullDataChamp!.passive.imageURL))
+										  .frame(width: 80)
+										Spacer()
+										  .frame(width: 20)
+										VStack {
+											Text(d.fullDataChamp!.passive.name)
+											  .font(.system(size: 36, weight: .heavy, design: .default))
+											  .frame(maxWidth: .infinity, alignment: .topLeading)
+											Text(d.fullDataChamp!.passive.description)
+											  .font(.system(size: 14, weight: .medium, design: .default))
+											  .frame(maxWidth: .infinity, alignment: .topLeading)
+										}
+									}
+								}
+							}
+
+							VStack {
+								LazyVGrid(
+									columns: [GridItem(.flexible()), GridItem(.flexible())],
+									alignment: .center,
+									spacing: 7
+								) {
+									ForEach(Array(champion.stats.keys), id: \.self) { key in
 										HStack {
-											AsyncImage(url: URL(string: d.fullDataChamp!.passive.imageURL))
-											  .frame(width: 80)
 											Spacer()
-											  .frame(width: 20)
-											VStack {
-												Text(d.fullDataChamp!.passive.name)
-												  .font(.system(size: 36, weight: .heavy, design: .default))
-												  .frame(maxWidth: .infinity, alignment: .topLeading)
-												Text(d.fullDataChamp!.passive.description)
-												  .font(.system(size: 14, weight: .medium, design: .default))
-												  .frame(maxWidth: .infinity, alignment: .topLeading)
-											}
+											Text(key)
+											  .font(.system(size: 17, weight: .heavy, design: .default))
+											  .frame(width: 200, alignment: .leading)
+											Spacer()
 										}
+										Text(String(describing: champion.stats[key]!))
+										  .font(.system(size: 15, weight: .medium, design: .default))
+
 									}
 								}
+							}
+						}
+						  .frame(maxWidth: 900)
+						Spacer()
+					}
 
-								VStack {
-									LazyVGrid(
-										columns: [GridItem(.flexible()), GridItem(.flexible())],
-										alignment: .center,
-										spacing: 7
-									) {
-										ForEach(Array(champion.stats.keys), id: \.self) { key in
-											HStack {
-												Spacer()
-												Text(key)
-												  .font(.system(size: 17, weight: .heavy, design: .default))
-												  .frame(width: 200, alignment: .leading)
-												Spacer()
-											}
-											Text(String(describing: champion.stats[key]!))
-											  .font(.system(size: 15, weight: .medium, design: .default))
-
-										}
-									}
-								}
+					if (d.fullDataChamp != nil) {
+						HStack {
+							Spacer()
+							VStack {
+								Text("Skins")
+								  .font(.system(size: 34, weight: .heavy, design: .default))
+								  .frame(maxWidth: .infinity, alignment: .topLeading)
 							}
 							  .frame(maxWidth: 900)
 							Spacer()
 						}
+						HStack {
+							ScrollView(.horizontal) {
+								HStack {
+									ForEach(d.fullDataChamp!.skins, id: \.name.self) { skin in
+										let dta = NetworkImageData(ns: ns, id: champion.id)
 
-						if (d.fullDataChamp != nil) {
-							HStack {
-								Spacer()
-								VStack {
-									Text("Skins")
-									  .font(.system(size: 34, weight: .heavy, design: .default))
-									  .frame(maxWidth: .infinity, alignment: .topLeading)
-								}
-								  .frame(maxWidth: 900)
-								Spacer()
-							}
-							HStack {
-								ScrollView(.horizontal) {
-									HStack {
-										ForEach(d.fullDataChamp!.skins, id: \.name.self) { skin in
-											let dta = NetworkImageData(ns: ns, id: champion.id)
+										let _ = cache.downloadOrUseCache(url: skin.imageURL) { data in
+											dta.data = data
+										}
 
-											let _ = cache.downloadOrUseCache(url: skin.imageURL) { data in
-												dta.data = data
-											}
+										let height = 400.0
 
-											let height = geo.size.height / 2 < 400 ? 400 : geo.size.height / 2
+										VStack {
+											RawDataImage(data: dta, origin: nil)
+											  .frame(width: height * 1.69, height: height)
+											  #if os(macOS)
+											  .onTapGesture {
+												  let imageViewer = ImageViewer(dta: dta, title: skin.name == "default" ? champion.name : skin.name)
+												  imageViewer.openAsWindow()
+											  }
+											  #endif
 
-											VStack {
-												RawDataImage(data: dta, origin: nil)
-												  .frame(width: height * 1.69, height: height)
-												  #if os(macOS)
-												  .onTapGesture {
-													  let imageViewer = ImageViewer(dta: dta, title: skin.name == "default" ? champion.name : skin.name)
-													  imageViewer.openAsWindow()
-												  }
-												  #endif
+											Text(skin.name)
+											  .font(.system(size: 19, weight: .medium, design: .default))
+											  .frame(maxWidth: .infinity, alignment: .center)
 
-												Text(skin.name)
-												  .font(.system(size: 19, weight: .medium, design: .default))
-												  .frame(maxWidth: .infinity, alignment: .center)
-
-											}
+										}
 
 //                                                .clipped()
 //                                                .contentShape(Rectangle())
-										}
 									}
-									  .padding(Edge.Set.bottom, 17)
 								}
+								  .padding(Edge.Set.bottom, 17)
 							}
-							  .padding(7)
 						}
-
+						  .padding(7)
 					}
+
 				}
 			}
 		}
